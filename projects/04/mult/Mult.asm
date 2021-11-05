1fe3ab4be1e2276fd47f19834c5c7ac63023fd37
youliangdao <youliangdaojing5@gmail.com>
2021-11-05 13:35:44 +0900

    乗算プログラムの作成

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -1,3 +1,5 @@
+
+   
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
@@ -5,8 +7,25 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
-// This program only needs to handle arguments that satisfy
-// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    @R2
+    M=0 // R2=0
+    @i
+    M=1 // i=1
+(LOOP)
+    @i
+    D=M // D=i
+    @R1
+    D=D-M // D=i-R1
+    @END
+    D;JGT
+    @R0
+    D=M // D=R0
+    @R2
+    M=D+M // R2=R0+R2
+    @i
+    M=M+1 // i++
+    @LOOP
+    0;JMP
+(END)
+    @END
